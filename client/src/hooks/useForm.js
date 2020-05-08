3f73633f89c9bc481b6990740a9e36b3cffb912c
Devoughn Hall <devoughnhall@Devoughns-MacBook-Pro.local>
2020-05-08 13:46:36 -0400

    hook working

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+import React,{useState} from 'react'
+
+
+export const useForm = (values, setValues) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+   
+
+
+
+    return [ showSuccessMessage, setShowSuccessMessage,]
+}
